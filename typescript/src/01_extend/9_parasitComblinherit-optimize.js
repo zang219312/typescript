4a03e77591b50880023ea2f5f2df47983b7589b7
zhangding <2324674973@qq.com>
2022-08-03 20:56:35 +0800

    feat：寄生组合继承实现及优化

--- typescript/src/01_extend/9_parasitComblinherit-optimize.js
+++ typescript/src/01_extend/9_parasitComblinherit-optimize.js
@@ -14,22 +14,26 @@ function ChinesePeople(name, sex, phone, national) {
 }
 
 // 寄生组合继承实现
-// 1.创建一个寄生构造函数
-function Middle() {
-  this.count = 33
+// 1.构建一个共用的寄生组合继承函数
+function _extends(parent, son) {
+  function Middle() {
+    // 创建一个寄生构造函数
+    this.count = 33
+    this.constructor = son
+  }
+
+  Middle.prototype = parent.prototype
+  // 2.创建寄生函数的实例对象
+  return new Middle()
 }
 
-Middle.prototype = People.prototype
-// 2.创建寄生函数的实例对象
-const middle = new Middle()
 // 3.chinesePeople 的显示原型指向 第二步创建的实例对象
-ChinesePeople.prototype = middle
-ChinesePeople.prototype.constructor = ChinesePeople
-//
+ChinesePeople.prototype = _extends(People, ChinesePeople)
+// ChinesePeople.prototype.constructor = ChinesePeople
+
 const cp1 = new ChinesePeople('王博', '男', 123456, '汉族')
 const cp2 = new ChinesePeople('阿兰', '女', 456789, '壮族')
-
 // console.log(ChinesePeople.prototype.__proto__ === People.prototype)  // true
 // console.log(cp1.__proto__.__proto__ === People.prototype) // true
-console.log('p1', cp1)
+console.log('p1', ChinesePeople.prototype)
 console.log('p2', cp2)
