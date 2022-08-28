6fc6a4c18125ff16b14acc41fd462dbe36afc1da
zhangding <2324674973@qq.com>
2022-08-28 19:54:13 +0800

    fix:入口文件

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,14 +1,36 @@
-function greeter(person: any): string {
-  return 'Hello, ' + person
-}
+// import './01_extend/1_prototypeExtend.js'
+// import './01_extend/2_prototypeExtend.js'
+// import './01_extend/3_prototypeExtend.js'
+// import './01_extend/4_callapplybind.js'
+// import './01_extend/6_借用constructor.js'
+// import './01_extend/7_原型和构造函数组合.js'
+// import './01_extend/8_parasitComblinherit.js'
+// import './01_extend/9_parasitComblinherit-optimize'
+// import "./01_extend/10_ObjectCreate"
+// import "./01_extend/11_clone"
 
-const user = 'Type'
+// import './02_car/allVehicle'
+// import './02_car/01_setPrototypeof.js'
+// import './02_car/02_ObjectCreate&setPrototypeof'
+// import './02_car/03_staticMemberExtend'
+// import './02_car/04_useExtendStatic'
+// import './02_car/05_allExtend'
 
-greeter(user)
+// import './03_类型断言/01_断言子类和父类继承'
+// import './03_类型断言/02_断言子类和父类没有继承'
 
-console.log('abc')
-let count: number = 16
-console.log('count: ', count)
+// import './04_类型守卫/01_new对象底层发生了什么'
+// import './04_类型守卫/02_类型断言'
+// import './04_类型守卫/03_typeof局限性'
 
-// 原型测试
-// import './test'
+// import './04_类型守卫/04_instanceof准备'
+
+// import './04_类型守卫/05_instanceof'
+// import　'./04_类型守卫/06_override多态1'
+// import './04_类型守卫/07_abstract1'
+// import './04_类型守卫/08_自定义守卫'
+// import './04_类型守卫/10_variabletuple'
+// import './04_类型守卫/11_variabletuple'
+
+// import './05_泛型/01_ArrayList'
+import './05_泛型/02_ArrayList'
