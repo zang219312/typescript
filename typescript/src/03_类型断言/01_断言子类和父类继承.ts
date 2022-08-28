45c8c2e9f9900536bc44fc9483c021a145fca9ea
zhangding <2324674973@qq.com>
2022-08-28 19:52:38 +0800

    feat:类型断言

---
+++ typescript/src/03_类型断言/01_断言子类和父类继承.ts
@@ -0,0 +1,50 @@
+class People {
+  public myusername!: string;
+  public myage!: number;
+  public address!: string
+
+  constructor() {
+  }
+
+  eat() {
+  }
+
+  step() {
+    console.log("People=>step");
+  }
+
+}
+
+class Stu extends People {
+  public username!: string
+  public age!: number;
+  public address!: string// 类型 "Stu" 中缺少属性 "address"，但类型 "typestu2" 中需要该属性。t
+  constructor(username: string, age: number, address: string, public phone: string) {
+    //  super(username, age);
+    super();
+    this.address = address;
+  }
+
+  study() {
+  }
+
+  eat() {
+  }
+}
+
+
+const people = new People()
+// const result = people as Stu // 正确 类型断言
+const result = <Stu>people // 正确 类型转换
+result.study()
+
+const stu = new Stu('王玉', 26, '北京', "123")
+const result2 = stu as People
+result2.step()
+export {}
+// 类型断言中的不能相互重叠问题:
+//   两个类之间断言的规则:
+//   两个类中任意一个的属性和方法是另一个类的属性和方法完全相同或子集，则这两个类可以相互断言
+//   否则这两个类就不能相互断言
+
+
